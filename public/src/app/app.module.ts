35acc4c59b50d40c15bb5273583000395f6075f3
榊原昌彦 <sakakibara@rdlabo.jp>
2019-12-09 15:21:39 +0900

    formatter

--- public/src/app/app.module.ts
+++ public/src/app/app.module.ts
@@ -4,12 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { IonicModule } from '@ionic/angular';
 
 import { AppComponent } from './app.component';
+import { ChartComponent } from './chart/chart.component';
 
 @NgModule({
-  declarations: [AppComponent],
+  declarations: [AppComponent, ChartComponent],
   entryComponents: [],
   imports: [BrowserModule, IonicModule.forRoot()],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
---
+++ public/src/app/chart/chart.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, AfterViewInit, ViewChild, ElementRef, Input } from '@angular/core';
+import { Chart, ChartData, ChartOptions } from 'chart.js';
+
+@Component({
+  selector: 'app-chart',
+  templateUrl: './chart.component.html',
+  styleUrls: ['./chart.component.scss'],
+})
+export class ChartComponent implements OnInit, AfterViewInit {
+  @ViewChild('canvas', { static: false })
+  ref: ElementRef;
+
+  @Input()
+  data: ChartData;
+
+  @Input()
+  options: ChartOptions;
+
+  context: CanvasRenderingContext2D;
+  chart: Chart;
+
+  constructor(private _elementRef: ElementRef) {}
+
+  ngOnInit() {}
+
+  ngAfterViewInit() {
+    this.context = this.ref.nativeElement.getContext('2d');
+    this.chart = new Chart(this.context, {
+      type: 'doughnut',
+      data: this.data,
+      options: this.options,
+    });
+  }
+}
